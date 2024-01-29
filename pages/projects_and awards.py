4378892106d53b3e137df5f0a53f6ede52c0f175
Matthew Ogbuehi <38775809+mogbuehi@users.noreply.github.com>
2024-01-29 06:19:07 -0800

    Update projects_and awards.py

--- pages/projects_and awards.py
+++ pages/projects_and awards.py
@@ -1,14 +1,27 @@
 import streamlit as st
 
 projects = {
-    "🏆Job Seeker Workbook": "",
+    "Mistral AI API Chatbot": "https://github.com/mogbuehi/Mistral_AI_Chatbot",
+    
+    " \"Chat with YouTube\" application" : {
+        "Colab Notebook": "https://github.com/mogbuehi/chat_with_yt/blob/main/Chat_with_YT_version_2.ipynb",
+        "Workshop with Tina Huang": "https://www.youtube.com/watch?v=mHbnEFxaDs4"
+    },
+    
+    "🏆Job Seeker Workbook": {
+        "Workbook": "https://lastmileai.dev/workbooks/cln0k7vr4018lpew58p32ptny",
+        "Winning Announcement": "https://www.linkedin.com/feed/update/urn:li:activity:7116172175723696128/"
+    },
+
+    "AI YouTube Language Tutor (StreamLit AI Hackathon, 2023)": "https://huggingface.co/spaces/mogbuehi/YT_Language_Tutor",
+    
     "AI Tutor for Data Science (Freelance Project for Statascratch)": {"Demo": "https://huggingface.co/spaces/mogbuehi/LO_AI_Tutor", 
                                                                        "Video Presentation" : "https://drive.google.com/file/d/1UHJvTDQ2SdpPJGkpwzs0AGnMplx_mdiP/view?usp=drive_link", 
                                                                        "Slide Deck": "https://docs.google.com/presentation/d/1kH8TqgbvjcZ-57-MGf6eioZ6nHlKnG7DwKd8ShbJJNs/edit?usp=drive_link"
-},
-    "AI YouTube Language Tutor (StreamLit AI Hackathon, 2023)": "",
-    "LLM classifier for generating synthetic data (Brembo Generative AI Hackathon, 2023)": "",
-    "Mistral AI API Chatbot": "https://github.com/mogbuehi/Mistral_AI_Chatbot"
+                                                                      },
+    
+    "LLM classifier for generating synthetic data (Brembo Generative AI Hackathon, 2023)": "https://colab.research.google.com/drive/1-Pt8WuLWiSjEqWcaN5KvxXpUs5fdb7uz?usp=sharing",
+    
 }
 
 # --- Projects ---
